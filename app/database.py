2353d761cb3be1c75666278f5be89390457bb4d7
신승민 <tlstmdals05@gmail.com>
2025-06-08 16:41:21 +0900

    issue_testing

--- app/database.py
+++ app/database.py
@@ -1,10 +1,21 @@
+import os
+from dotenv import load_dotenv
 from databases import Database
 from sqlalchemy import create_engine, MetaData
 import pymysql
 pymysql.install_as_MySQLdb()
 
+load_dotenv()
+
 # MariaDB 연결 정보 설정
-DATABASE_URL = "mysql+aiomysql://root:root@localhost:3310/shin"
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
+
+DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # databases용 비동기 연결
 database = Database(DATABASE_URL)
