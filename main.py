ff48f5f1623db5e0acd2ac18909ea6540b617d80
root-dove <76778734+root-dove@users.noreply.github.com>
2025-06-11 10:36:36 +0900

    cors설정

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from fastapi import FastAPI
 from app.database import database, engine  # engine은 여기서 가져와야 함
 from app.models import metadata            # metadata만 models.py에서 가져오면 됨
 from app.routers import user, project, team, auth, sprint, comment, alert, issue
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
+# ✅ CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 # ✅ DB 연결
 @app.on_event("startup")
